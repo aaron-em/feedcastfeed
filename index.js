20392170e55d1d08095ad15d255463d26f1d5e52
Aaron Miller <me@aaron-miller.me>
2016-06-28 15:28:31 -0400

    now it makes wav files! ^___^

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+var config = require('config');
 var util = require('util');
+
 var FetchStream = require('./lib/fetch-stream');
+var TTSStream = require('./lib/tts-stream');
 
 var entries = new FetchStream("https://stratechery.com/feed/");
+var tts = new TTSStream(config.paths.tts, config.paths.temp);
 
-entries.on('data', function(item) {
-  console.log(util.inspect(item));
-});
+entries.pipe(tts);
+
+// entries.on('data', function(item) {
+//   console.log(util.inspect(item));
+// });
 
 entries.go();
